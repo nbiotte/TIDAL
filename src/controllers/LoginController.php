f61fceb3e2d8b9cada40956566673e3ac39b0377
nbiotte <nathan.biotte@soprasteria.com>
2020-11-11 12:42:42 +0100

    Fix mistake in LoginController

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -30,11 +30,12 @@ class LoginController{
         //print_r($_POST);
         $pwd = md5($data['pwd']);
         $customer = $connexionService->getCustomer(['id','firstname', 'familyname', 'address'], ["username = '".$data['username']."' AND password = '$pwd'" ]);
-        if (!empty($customer)) 
+        if (!empty($customer)) {
             $_SESSION["id"]=$customer[0][0];
             $_SESSION["firstname"]=$customer[0][1];
             $_SESSION["familyname"]=$customer[0][2];
             $_SESSION["address"]=$customer[0][3];
+        }
     }
 }
 
